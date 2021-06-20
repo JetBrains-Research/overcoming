438075b4d144b12a063832db3dfd5aeed3196ef5
Agniya Serheyuk <sernijagnejuk@gmail.com>
2021-06-20 20:54:51 +0200

    two pages with error in user db

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField
 from wtforms import SubmitField
+from wtforms import StringField
+from wtforms import RadioField
+from wtforms.validators import DataRequired
 import json
 
 app = Flask(__name__)
@@ -18,9 +21,21 @@ db = SQLAlchemy(app)
 class Answers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     answer = db.Column(db.String(300), index=True, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, answer):
         self.answer = answer
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    theme = db.Column(db.String(64), index=True, unique=False)
+    answer = db.relationship('Answers', backref='user', lazy='dynamic')
+
+    def __init__(self, username, theme):
+        self.username = username
+        self.theme = theme
 
 
 db.create_all()
@@ -35,13 +50,38 @@ with open('tasks.json') as tasks_json:
     tasks_list = json.load(tasks_json)
 
 
+class UserForm(FlaskForm):
+    theme_categories = [("Light", "Light"), ("Dark", "Dark")]
+    user_name = StringField("User name", validators=[DataRequired()])
+    theme = RadioField("Your usual theme", choices=theme_categories)
+    submit = SubmitField("Submit")
+
+
+@app.route('/user', methods=["GET", "POST"])
+def user():
+    user_form = UserForm(csrf_enabled=False)
+    if user_form.validate_on_submit():
+        user_name = request.form.get('user_name', False)
+        new_user_name = User(username=user_name)
+        db.session.add(new_user_name)
+        db.session.commit()
+
+        theme = request.form.get('theme', False)
+        u_theme = User(theme=theme)
+        db.session.add(u_theme)
+        db.session.commit()
+
+    return render_template('user.html',
+                           template_form=user_form)
+
+
 @app.route('/task', methods=["GET", "POST"])
 def task():
     task_line1 = tasks_list['task'][0]['line1']
     task_line2 = tasks_list['task'][0]['line2']
 
     answer = request.form.get('answer', False)
-    new_answer = Answers(answer)
+    new_answer = Answers(answer=answer)
     db.session.add(new_answer)
     db.session.commit()
 
